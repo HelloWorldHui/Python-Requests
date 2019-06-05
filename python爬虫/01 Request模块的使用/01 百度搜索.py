dd753407b3145d1a98d4c3ee4806c1e1410671b6
hui <1493455610@qq.com>
2019-06-05 21:25:44 +0800

    百度搜索,UA(User-Agent)反爬

--- python爬虫/01 Request模块的使用/01 百度搜索.py
+++ python爬虫/01 Request模块的使用/01 百度搜索.py
@@ -7,9 +7,9 @@ import requests
 url = "https://www.baidu.com/s"
 # 请求头,带参数  百度搜索url
 
-result = requests.get(url=url,params={"wd":"python"},headers={"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.121 Safari/537.36"})
+response = requests.get(url=url,params={"wd":"python"},headers={"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.121 Safari/537.36"})
 # wd 搜索的关键字  UA(User-Agent)反爬
 
 with open("baidu.html","wb") as f:
-    f.write(result.content)
+    f.write(response.content)
 # result.content bytes形式
